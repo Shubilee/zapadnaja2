5aecf4ab51e77a537e2b4252f6a8abeb27bdfd3a
Wadimx199789 <martinkewichwadim@gmail.com>
2022-06-03 17:32:37 +0300

    feat: add about section

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,31 @@
 $(function() {
+	$(".slider-noDots").slick({
+		speed:700,
+		mobileFirst: true,
+		dots:false,
+		slidesToShow:1,
+		slidesToScroll:1,
+		pauseOnHover:false,
+		respondTo:'min',
+		cssEase:'linear',
+		prevArrow:'<div class="prevArrow"></div>',
+		nextArrow:'<div class="nextArrow"></div>', 
+		arrows:false,
+		responsive: [
+		  {
+			breakpoint: 768,
+			settings: {
+			  arrows : true,
+			}
+		  }
+		  // You can unslick at a given breakpoint now by adding:
+		  // settings: "unslick"
+		  // instead of a settings object
+		]
+	  });
   
 	$(".slider__container").slick({
-	//   autoplay: true,
+	  autoplay: true,
 	  autoplaySpeed:3000,
 	  speed:700,
 	  mobileFirst: true,
@@ -25,7 +49,9 @@ $(function() {
 		// settings: "unslick"
 		// instead of a settings object
 	  ]
-	})
+	});
+
+
 	$('.popup-with-form').magnificPopup({
 		type: 'inline',
 		focus: '#name'
